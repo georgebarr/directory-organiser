777bc95a0d0e2c0a66b355429e6e2861c85b1136
George <110062762+georgebarr@users.noreply.github.com>
2024-08-16 21:35:20 +0100

    initial commit

---
+++ file_extensions.py
@@ -0,0 +1,11 @@
+images = [".jpg", ".jpeg", ".png", ".gif", ".svg", ".tiff", ".bmp", ".webp"]
+
+videos = [".mp4", ".mov", ".avi", ".mkv", "flv", ".wmv", ".webm"]
+
+music = [".mp3", ".wav", ".flac", ".aac", ".ogg", ".m4a"]
+
+documents = [".pdf", ".docx", ".doc", ".xlsx", ".xls", ".txt", ".rtf"]
+
+design_files = [".psd", ".ai", ".indd", ".sketch", ".blend", ".obj", ".fbx"]
+
+code = [".py", ".c", ".js", ".css", ".html", ".java", ".xml", ".cpp", ".json"]
